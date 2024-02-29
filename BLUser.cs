e9a71f87603ed6c734251f4e448106ad54695f3b
Npota90 <158215220+Npota90@users.noreply.github.com>
2024-02-29 17:07:13 +0530

    Add files via upload

---
+++ BLUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTeachingProject.App_Code.BAL
+{
+    public class BLUser
+    {
+        public string home { get; set; }
+        public string registration { get; set; }
+        public string feedback { get; set; }
+        public string staff_report { get; set; }
+        public string download { get; set; }
+        public string contact_us { get; set; }
+    }
+}
---
+++ DBConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTeachingProject.App_Code.DAL
+{
+    public class DBConnection
+    {
+        
+    }
+}
